7d4c83291dbe314054cda4255d924604e465070a
Sebastien Beauvais <sbeauvais7@gmail.com>
2023-05-29 10:10:00 -0600

    adding files and references

---
+++ se_projects/week1/Models/CouponList.cs
@@ -0,0 +1,4 @@
+﻿// coupon class
+public class CouponList {
+    // functions and methods for Coupon List logic
+}
---
+++ se_projects/week1/Models/Item.cs
@@ -0,0 +1,4 @@
+﻿// coupon class
+public class Item {
+    // functions and methods for Item logic
+}
